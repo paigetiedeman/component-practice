f635d2a1ac7351a1d17b7ed3d4c32ccddc4800af
Paige Tiedeman <paigetiedeman@gmail.com>
2022-01-15 00:32:54 -0800

    Styling cards and Input binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { title: 'Neat Tree', username: 'nature', imageUrl: 'assets/tree.jpeg', content: 'I saw this neat tree today' },
+    { title: 'Snowy Mountain', username: 'mountainlover', imageUrl: 'assets/mountain.jpeg', content: 'Here is a picture of a snowy mountain' },
+    { title: 'Mountain Biking', username: 'biking122', imageUrl: 'assets/biking.jpeg', content: 'I did some biking today' }
+  ]
 }
